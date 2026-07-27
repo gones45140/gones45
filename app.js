d7f2210223fa27f60a873e43756ea540afb327bb
gones45140 <touraine.antoine@orange.fr>
2026-07-27 11:58:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -22224,6 +22224,8 @@ function _g45MotoTrack(t){ if(!t) return ''; var k=String(t).toLowerCase().trim(
 var _G45_CY_RACES=[
   {id:'tdf', n:'Tour de France', host:'letour', flag:'🇫🇷', rc:'racecenter.letour.fr'},
   {id:'tdff', n:'Tour Femmes', flag:'🇫🇷', rc:'racecenter.letourfemmes.fr'},
+  {id:'pn', n:'Paris-Nice', flag:'🇫🇷', rc:'racecenter.paris-nice.fr'},
+  {id:'dauphine', n:'Dauphiné', flag:'🇫🇷', rc:'racecenter.criterium-du-dauphine.fr'},
   {id:'vuelta', n:'La Vuelta', host:'vuelta', flag:'🇪🇸', rc:'racecenter.lavuelta.es'}
 ];
 function _g45CyEa(s){ return String(s==null?'':s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/"/g,'&quot;'); }
